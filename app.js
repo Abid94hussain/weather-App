98d7c28d6f64538fe9a66870ba86719d0be19a5e
Abid Hussain <abidhussain949936@gmail.com>
2023-11-25 11:56:51 +0530

    some changes

---
+++ app.js
@@ -0,0 +1,42 @@
+let inpt = document.querySelector("#inpt");
+let submit = document.querySelector(".submit");
+let temp = document.querySelector(".temp");
+let wind = document.querySelector(".wind");
+let description = document.querySelector(".description");
+
+let tempfor1 = document.querySelector(".temp-for1");
+let windfor1 = document.querySelector(".wind-for1");
+
+let tempfor2 = document.querySelector(".temp-for2");
+let windfor2 = document.querySelector(".wind-for2");
+
+let tempfor3 = document.querySelector(".temp-for3");
+let windfor3 = document.querySelector(".wind-for3");
+
+let url = "https://goweather.herokuapp.com/weather/";
+
+submit.addEventListener("click", async function () {
+     let input = inpt.value;
+     let Data = await getData(input);
+     console.log(Data);
+     temp.innerText = Data.data.temperature;
+     wind.innerText = Data.data.wind;
+     description.innerText = Data.data.description;
+
+     tempfor1.innerText = Data.data.forecast[0].temperature;
+     windfor1.innerText = Data.data.forecast[0].wind;
+     
+     tempfor2.innerText = Data.data.forecast[1].temperature;
+     windfor2.innerText = Data.data.forecast[1].wind;
+     
+     tempfor3.innerText = Data.data.forecast[2].temperature;
+     windfor3.innerText = Data.data.forecast[2].wind;
+
+     inpt.value = "";
+})
+
+async function getData(input) {
+     let res = await axios.get(url + input);
+     return res;
+}
+
