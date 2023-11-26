7f37ec3d2657534c5f9de5be7023aa18af074e15
Abid Hussain <abidhussain949936@gmail.com>
2023-11-26 11:59:49 +0530

    some more changes

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ let submit = document.querySelector(".submit");
 let temp = document.querySelector(".temp");
 let wind = document.querySelector(".wind");
 let description = document.querySelector(".description");
+let weatherPlace = document.querySelector(".weatherPlace");
 
 let tempfor1 = document.querySelector(".temp-for1");
 let windfor1 = document.querySelector(".wind-for1");
@@ -16,24 +17,27 @@ let windfor3 = document.querySelector(".wind-for3");
 let url = "https://goweather.herokuapp.com/weather/";
 
 submit.addEventListener("click", async function () {
-     let input = inpt.value;
-     let Data = await getData(input);
-     console.log(Data);
-     temp.innerText = Data.data.temperature;
-     wind.innerText = Data.data.wind;
-     description.innerText = Data.data.description;
+     try {
+          let input = inpt.value;
+          let Data = await getData(input);
 
-     tempfor1.innerText = Data.data.forecast[0].temperature;
-     windfor1.innerText = Data.data.forecast[0].wind;
-     
-     tempfor2.innerText = Data.data.forecast[1].temperature;
-     windfor2.innerText = Data.data.forecast[1].wind;
-     
-     tempfor3.innerText = Data.data.forecast[2].temperature;
-     windfor3.innerText = Data.data.forecast[2].wind;
+          weatherPlace.innerText = `Weather of : ${inpt.value}`;
+          temp.innerText = Data.data.temperature;
+          wind.innerText = Data.data.wind;
+          description.innerText = Data.data.description;
 
-     inpt.value = "";
-})
+          tempfor1.innerText = Data.data.forecast[0].temperature;
+          windfor1.innerText = Data.data.forecast[0].wind;
+
+          tempfor2.innerText = Data.data.forecast[1].temperature;
+          windfor2.innerText = Data.data.forecast[1].wind;
+
+          tempfor3.innerText = Data.data.forecast[2].temperature;
+          windfor3.innerText = Data.data.forecast[2].wind;
+     } catch {
+          inpt.value = ""; weatherPlace.innerText = "404 Not found";
+     }
+});
 
 async function getData(input) {
      let res = await axios.get(url + input);
